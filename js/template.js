7d22c774d20e4f9ccaab0863cc9315212520e119
Andreas Kournoutas <andreaskournoutas@gmail.com>
2021-12-31 13:19:16 +0200

    FB chat plugin

---
+++ js/template.js
@@ -0,0 +1,23 @@
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-V5HP7Y1MMT');
+
+var chatbox = document.getElementById('fb-customer-chat');
+chatbox.setAttribute("page_id", "1696473387271743");
+chatbox.setAttribute("attribution", "biz_inbox");
+
+window.fbAsyncInit = function() {
+    FB.init({
+        xfbml: true,
+        version: 'v12.0'
+    });
+};
+
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/el_GR/sdk/xfbml.customerchat.js';
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
