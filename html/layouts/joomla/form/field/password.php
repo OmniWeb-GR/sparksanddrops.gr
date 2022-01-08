abb1c6d4fd614362e1afdd875218a466e85be8a7
Andreas Kournoutas <andreaskournoutas@gmail.com>
2022-01-08 18:30:21 +0200

    Login form changes

--- html/layouts/joomla/form/field/password.php
+++ html/layouts/joomla/form/field/password.php
@@ -152,15 +152,15 @@ if ($rules)
 			id="<?php echo $id; ?>"
 			value="<?php echo htmlspecialchars($value, ENT_COMPAT, 'UTF-8'); ?>"
 			<?php echo implode(' ', $attributes); ?>>
-		<?php if (!$lock): ?>
-		<button type="button" class="btn btn-secondary input-password-toggle">
+		<?php // if (!$lock): ?>
+		<!-- <button type="button" class="btn btn-secondary input-password-toggle">
 			<span class="icon-eye icon-fw" aria-hidden="true"></span>
 			<span class="visually-hidden"><?php echo Text::_('JSHOWPASSWORD'); ?></span>
-		</button>
-		<?php else: ?>
-			<button type="button" id="<?php echo $id; ?>_lock" class="btn btn-info input-password-modify locked">
+		</button> -->
+		<?php // else: ?>
+			<!-- <button type="button" id="<?php echo $id; ?>_lock" class="btn btn-info input-password-modify locked">
 				<?php echo Text::_('JMODIFY'); ?>
-			</button>
-		<?php endif; ?>
+			</button> -->
+		<?php // endif; ?>
 	</div>
 </div>
