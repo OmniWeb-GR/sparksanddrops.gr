d1c4803ca808984909c7e8615cc57014574f8dcb
Andreas Kournoutas <andreaskournoutas@gmail.com>
2022-01-08 14:06:33 +0200

    Update default.php

--- html/mod_articles_latest/default.php
+++ html/mod_articles_latest/default.php
@@ -22,20 +22,20 @@ if (!$list)
 	<div class="col-6 col-sm-6 col-md-4 col-lg-3" itemscope itemtype="https://schema.org/Article">
 		<div class="card bg-transparent border-light h-100">
 			<div class="ratio ratio-4x3">
-				<?php if (LayoutHelper::render('joomla.content.intro_image', $this->item)) : ?>
-					<?php echo LayoutHelper::render('joomla.content.intro_image', $this->item); ?>
+				<?php if (LayoutHelper::render('joomla.content.intro_image', $item)) : ?>
+					<?php echo LayoutHelper::render('joomla.content.intro_image', $item); ?>
 				<?php else : ?>
-					<?php if (LayoutHelper::render('joomla.content.full_image', $this->item)) : ?>
+					<?php if (LayoutHelper::render('joomla.content.full_image', $item)) : ?>
 						<?php if ($params->get('link_intro_image') && ($params->get('access-view') || $params->get('show_noauth', '0') == '1')) : ?>
-							<a class="bg-light rounded text-center" href="<?php echo Route::_(RouteHelper::getArticleRoute($this->item->slug, $this->item->catid, $this->item->language)); ?>">
-								<?php echo LayoutHelper::render('joomla.content.full_image', $this->item); ?>
+							<a class="bg-light rounded text-center" href="<?php echo Route::_(RouteHelper::getArticleRoute($item->slug, $item->catid, $item->language)); ?>">
+								<?php echo LayoutHelper::render('joomla.content.full_image', $item); ?>
 							</a>
 						<?php else : ?>
-							<?php echo LayoutHelper::render('joomla.content.full_image', $this->item); ?>
+							<?php echo LayoutHelper::render('joomla.content.full_image', $item); ?>
 						<?php endif; ?>
 					<?php else : ?>
 						<?php if ($params->get('link_intro_image') && ($params->get('access-view') || $params->get('show_noauth', '0') == '1')) : ?>
-							<a class="bg-light rounded text-center" href="<?php echo Route::_(RouteHelper::getArticleRoute($this->item->slug, $this->item->catid, $this->item->language)); ?>">
+							<a class="bg-light rounded text-center" href="<?php echo Route::_(RouteHelper::getArticleRoute($item->slug, $item->catid, $item->language)); ?>">
 								<figure class="figure mb-0 h-100">
 									<img class="figure-img img-fluid rounded mb-0" src="/images/logo.webp" alt="Sparks & Drops" itemprop="image" width="500" height="500" loading="lazy">
 								</figure>
