77896136e1eb0348f87b9b0af654b322e8122625
Andreas Kournoutas <andreaskournoutas@gmail.com>
2022-01-09 23:25:51 +0200

    Update default_login.php

--- html/com_users/login/default_login.php
+++ html/com_users/login/default_login.php
@@ -59,13 +59,11 @@ $usersConfig = ComponentHelper::getParams('com_users');
 			<?php endif; ?>
 
 			<?php if (PluginHelper::isEnabled('system', 'remember')) : ?>
-				<div class="com-users-login__remember">
-					<div class="form-check">
-						<input class="form-check-input" id="remember" type="checkbox" name="remember" value="yes">
-						<label class="form-check-label" for="remember">
-							<?php echo Text::_('COM_USERS_LOGIN_REMEMBER_ME'); ?>
-						</label>
-					</div>
+				<div class="form-check mb-3">
+					<input class="form-check-input" id="remember" type="checkbox" name="remember" value="yes">
+					<label class="form-check-label" for="remember">
+						<?php echo Text::_('COM_USERS_LOGIN_REMEMBER_ME'); ?>
+					</label>
 				</div>
 			<?php endif; ?>
 
